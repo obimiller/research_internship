6803ac606ad44db401ad1d166075611a9b27aa72
Oberon Miller <drwhotimelord08@gmail.com>
2025-07-17 22:34:24 -0700

    fix plotting of PO and PA data; generate a few species maps

--- dispo.r
+++ dispo.r
@@ -7,18 +7,19 @@ x11()
 
 # one of AWT, CAN, NSW, NZ, SA, SWI
 # there is no presence absence data for CAN or SWI or SA
-region <- "AWT"
+region <- "SWI"
+species_num <- "01"
 
 # combine the presence and background points (got this from code example in PDF in Appendix 2)
 
 # loading the presence-background data
-pr <- disPo(region)
+presence_only_data <- disPo(region)
 bg <- disBg(region) # this is 10,000 random backgrounds
 
-training <- rbind(pr, bg)
+# print(head(presence_only_data, 100))
 
-# create the sf object using the coordinates in the training data (this accepts UTM and lat/long)
-training_sf_obj <- st_as_sf(training, coords = c("x", "y"), crs = 27200)
+# create the sf object using the coordinates in the presence-only data (this accepts UTM and lat/long)
+presence_only_obj <- st_as_sf(presence_only_data, coords = c("x", "y"), crs = 27200)
 
 # the border geometry for the specified country as stored in the disdat library
 region_border <- disBorder(region)
@@ -27,7 +28,8 @@ region_border <- disBorder(region)
 
 # NSW groups -> ba db nb ot ou rt ru sr
 # AWT groups -> "bird"	Birds "mam"	Mammals "ru" Reptiles "fro"	Frogs "plant"
-testing_pa <- disPa(region, group="bird")
+# testing_pa <- disPa(region, group="ba")
+testing_pa <- disPa(region)
 
 # create the sf object using the coordinates in the testing data (this accepts UTM and lat/long)
 testing_sf_obj <- st_as_sf(testing_pa, coords = c("x", "y"), crs = 27200)
@@ -35,26 +37,50 @@ testing_sf_obj <- st_as_sf(testing_pa, coords = c("x", "y"), crs = 27200)
 # get the names of all species columns
 species_names <- names(testing_sf_obj)[!names(testing_sf_obj) %in% attr(testing_sf_obj, "sf_column")]
 
-# just to see what the training data columns and species names are for a given region
-print(names(training))
-print(species_names)
+# just to see what the presence_only data columns and species names are for a given region
+# print(names(presence_only_data))
+# print(species_names)
 
-# function to plot species and training data
-plot_pa_species_and_training_data <- function(species_id, data_name) {
+# function to plot species and presence-only data
+plot_pa_species_and_po_data <- function(species_id, data_name, region) {
+    par(bg = "white")
 
-  # makes the data for P/A a diamond
-    species_shape <- 23
-  
+    # this is to make sure the legend doesn't get cut off for SWI
+    if (region == "SWI") {
+      par(mar = c(0, 0, 20, 0))  # add more space to the right
+    }
+
     plot(st_geometry(region_border), col = "white", border = "black", main = species_id)
-    plot(training_sf_obj[data_name], add= TRUE, cex = 0.3)
-    plot(testing_sf_obj[species_id], pch = species_shape, bg = "red", add = TRUE, cex = 1.5)
+
+    # these are just the points for the presence-only data
+    coords_po <- st_coordinates(presence_only_obj[presence_only_obj$spid == species_id, ])
+    points(coords_po, col = "forestgreen", pch = 23, cex = 1)
+
+    # this is the presence/absence data depending on whether it's 0 or 1
+    coords_pa <- st_coordinates(testing_sf_obj[species_id])
+    pa_values <- testing_sf_obj[[species_id]]
+
+    colors_pa <- ifelse(pa_values > 0.5, "red", "blue")
+
+    points(coords_pa, col = colors_pa, pch = 17, cex = 0.3)
+
+    legend("topright",
+       legend = c("Absent (0)", "Present (1)", "Presence Only"),
+       pch = c(16, 16, 23),
+       col = c("blue", "red", "forestgreen"),
+       title = paste("Species", species_id, sep=": "))
+
+    # this saves the charts to disk
+    # file_name <- paste(species_id, "png", sep=".")
+    # dev.copy(png, filename = file_name, width = 800, height = 600)
+    # dev.off()
 }
 
 # format species name according to region
-species <- tolower(paste(region, "02", sep=""))
+species <- tolower(paste(region, species_num, sep=""))
 
-# create map with training and testing data
-plot_pa_species_and_training_data(species, "slope")
+# create map with presence-only and testing data
+plot_pa_species_and_po_data(species, "spid", region)
 
 
 # keep the window open until the program ends
