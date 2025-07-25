29f764678f00935254de250c82dc88ca16364f6f
Oberon Miller <drwhotimelord08@gmail.com>
2025-07-24 21:03:09 -0700

    added a scale legend; changed colors for p/a plot

--- dispo.r
+++ dispo.r
@@ -1,13 +1,14 @@
 library(disdat)
 library(sf)
-library(ggplot2)
+# library(ggplot2)
+library(prettymapr)
 
 # create windows (on Linux that my dad helped me set up)
 x11()
 
 # one of AWT, CAN, NSW, NZ, SA, SWI
 # there is no presence absence data for CAN or SWI or SA
-region <- "SWI"
+region <- "CAN"
 species_num <- "01"
 
 # combine the presence and background points (got this from code example in PDF in Appendix 2)
@@ -39,7 +40,7 @@ species_names <- names(testing_sf_obj)[!names(testing_sf_obj) %in% attr(testing_
 
 # just to see what the presence_only data columns and species names are for a given region
 # print(names(presence_only_data))
-# print(species_names)
+print(species_names)
 
 # function to plot species and presence-only data
 plot_pa_species_and_po_data <- function(species_id, data_name, region) {
@@ -60,9 +61,16 @@ plot_pa_species_and_po_data <- function(species_id, data_name, region) {
     coords_pa <- st_coordinates(testing_sf_obj[species_id])
     pa_values <- testing_sf_obj[[species_id]]
 
+    coords_presence <- coords_pa[pa_values == 1, ]
+    coords_absence  <- coords_pa[pa_values == 0, ]
+
     colors_pa <- ifelse(pa_values > 0.5, "red", "blue")
 
-    points(coords_pa, col = colors_pa, pch = 17, cex = 0.3)
+    # points(coords_absence, col = rgb(0, 0, 1, 0.3), pch = 17, cex = 0.3)
+    points(coords_presence, col = "red", pch = 17, cex = 0.5)
+
+
+    # points(coords_pa, col = colors_pa, pch = 17, cex = 0.3)
 
     legend("topright",
        legend = c("Absent (0)", "Present (1)", "Presence Only"),
@@ -70,17 +78,26 @@ plot_pa_species_and_po_data <- function(species_id, data_name, region) {
        col = c("blue", "red", "forestgreen"),
        title = paste("Species", species_id, sep=": "))
 
+    addscalebar(pos = "bottomright", style = "bar", padin = c(0.5, 0.5), label.cex = 0.8)
+
     # this saves the charts to disk
-    # file_name <- paste(species_id, "png", sep=".")
-    # dev.copy(png, filename = file_name, width = 800, height = 600)
-    # dev.off()
+    file_name <- paste(species_id, "png", sep=".")
+    dev.copy(png, filename = file_name, width = 800, height = 600)
+    dev.off()
 }
 
-# format species name according to region
-species <- tolower(paste(region, species_num, sep=""))
+for (species_number in 1:20) {
+  # format species name according to region
 
-# create map with presence-only and testing data
-plot_pa_species_and_po_data(species, "spid", region)
+  if (species_number < 10) {
+    species_number <- paste("0", species_number, sep="")
+  }
+
+  species <- tolower(paste(region, species_number, sep=""))
+
+  # create map with presence-only and testing data
+  plot_pa_species_and_po_data(species, "spid", region)
+}
 
 
 # keep the window open until the program ends
